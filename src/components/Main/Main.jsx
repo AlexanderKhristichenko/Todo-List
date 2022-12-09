d693e6bd098b77f237d35a7f44ce7e390970bf91
Olexander.khrystychenko <alexander.khristichenko@gmail.com>
2022-12-09 09:59:42 +0200

    Form Components
    fix styles and adding Form component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,16 +2,16 @@ import Card from "../UI/Card";
 import styles from "./Main.module.css";
 import addList from "../../assets/images/Illustrations-reading-side.svg";
 
-const Main = ({ addNewList }) => {
+const Main = ({ isRes, addNewList }) => {
   return (
     <main className={styles.main}>
-      {!addNewList && (
+      {!isRes && (
         <Card className={styles.active}>
           <h1 className={styles.title}>to-do list</h1>
         </Card>
       )}
-      {addNewList && (
-        <Card className={styles.active}>
+      {isRes && (
+        <Card txt="Start creating lists (T_T)">
           <img className={styles.img} src={addList} alt="Add List" />
         </Card>
       )}
